7c23b80caf33dd47b907537b240da03b7c97eb93
Nivedita Gavade <nivedita.gavade19@gmail.com>
2023-05-23 11:02:34 +0530

    project upload

---
+++ src/BASIC/Method.java
@@ -0,0 +1,9 @@
+package BASIC;
+
+public class Method {
+	public static void main(String[] args) {
+		System.out.println("method is behaviour of class");
+		System.out.println("method can be non static also");
+	}
+
+}
---
+++ src/BASIC/Variables.java
@@ -0,0 +1,9 @@
+package BASIC;
+
+public class Variables {
+	public static void main(String[] args) {
+		System.out.println("Variable is property of class");
+		System.out.println("Variable can be static ");
+	}
+
+}
